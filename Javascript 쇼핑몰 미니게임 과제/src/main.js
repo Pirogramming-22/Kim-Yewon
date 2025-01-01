a4dffb3bddf79bde56401d9e1a894d05be4f873a
yeeeww <yeangsshi@ewhain.net>
2025-01-02 08:27:40 +0900

    JavaScript display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,11 +1,25 @@
 function loadItems() {
-  return fetch("data.data.json")
+  return fetch("./data/datajson")
     .then((response) => response.json())
     .then((json) => json.items);
 }
 
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+function createHTMLString(item) {
+  return `
+     <li class="item">
+          <img src="${item.image}" alt=${item.type} />
+          <span id="itemInfo">${item.gender}, ${item.size}</span>
+        </li>
+        `;
+}
+
 loadItems()
-  .then((itmes) => {
+  .then((items) => {
     displayItems(items);
     setEventListeners(items);
   })
