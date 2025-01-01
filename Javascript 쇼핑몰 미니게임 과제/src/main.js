197c616108672a50ffa8c5cdaab82e7da64671e8
yeeeww <yeangsshi@ewhain.net>
2025-01-02 08:44:34 +0900

    JavaScript handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -18,6 +18,25 @@ function createHTMLString(item) {
         `;
 }
 
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+  const filtered = items.filter((item) => item[key] === value);
+  displayItems(filtered);
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  logo.addEventListener("click", () => displayItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+}
+
 loadItems()
   .then((items) => {
     displayItems(items);
